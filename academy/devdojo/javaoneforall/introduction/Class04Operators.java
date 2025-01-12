6f9013e9e01ce97156438bfe60834189de296605
ruanwesleywr <ruanwesley682@gmail.com>
2025-01-12 18:55:08 -0300

    Operators

---
+++ academy/devdojo/javaoneforall/introduction/Class04Operators.java
@@ -0,0 +1,19 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class04Operators {
+    public static void main(String[] args) {
+        // +, -, /, *, %, <, >, <=, >=, !=
+        int number1 = 10;
+        int number2 = 20;
+        int subtractionResult = number1 - number2;
+        int addictionResult = number1 + number2;
+        int divisionResult = number2 / number1;
+        int multiplicationResult = number1 * number2;
+        int module = 20 % 2;
+        System.out.println(subtractionResult);
+        System.out.println(addictionResult);
+        System.out.println(divisionResult);
+        System.out.println(multiplicationResult);
+        System.out.println(module);
+    }
+}
