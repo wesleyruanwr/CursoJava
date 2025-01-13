ba48f159794d91033f580f5155408a2d1fb4fe46
ruanwesleywr <ruanwesley682@gmail.com>
2025-01-13 11:46:11 -0300

    Operators

--- academy/devdojo/javaoneforall/introduction/Class04Operators.java
+++ academy/devdojo/javaoneforall/introduction/Class04Operators.java
@@ -3,6 +3,10 @@ package academy.devdojo.javaoneforall.introduction;
 public class Class04Operators {
     public static void main(String[] args) {
         // +, -, /, *, %, <, >, <=, >=, !=
+
+        // logical operators
+        // && (and), || (or)
+
         int number1 = 10;
         int number2 = 20;
         int subtractionResult = number1 - number2;
@@ -15,5 +19,13 @@ public class Class04Operators {
         System.out.println(divisionResult);
         System.out.println(multiplicationResult);
         System.out.println(module);
+
+        int age = 35;
+        float salary = 3600F;
+        boolean isLegal = age >= 30 && salary>= 4612 || age >=31;
+        boolean isNotLegal = age < 30 && salary >= 3300 || salary >= 3500;
+        System.out.println(isLegal);
+        System.out.println(isNotLegal);
+
     }
 }
