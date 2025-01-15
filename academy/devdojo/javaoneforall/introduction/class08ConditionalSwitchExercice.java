b6a206b5a7d8f8ea516aea7a5338fa0a432bba1a
ruanwesleywr <ruanwesley682@gmail.com>
2025-01-15 16:12:12 -0300

    Switch Exercice

---
+++ academy/devdojo/javaoneforall/introduction/class08ConditionalSwitchExercice.java
@@ -0,0 +1,22 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class class08ConditionalSwitchExercice {
+    public static void main(String[] args) {
+        int weekDay = 4;
+        switch (weekDay){
+            case 7:
+            case 1:
+                System.out.println("Its not a bussiness day");
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Its a bussiness day");
+                break;
+            default:
+                System.out.println("Invalid Option");
+        }
+    }
+}
