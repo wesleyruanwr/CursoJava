ddbbc20c143007c30adb1f07c6141f1e879bca22
ruanwesleywr <ruanwesley682@gmail.com>
2025-01-20 23:01:14 -0300

    Loop Exercice

---
+++ academy/devdojo/javaoneforall/introduction/Class12LoopExercice3.java
@@ -0,0 +1,16 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class12LoopExercice3 {
+    public static void main(String[] args) {
+
+        int carPrice = 60000;
+        int totalPerMonth = 800;
+        int totalInstallments = 0;
+        int totalPayed = 0;
+        while(totalPayed<carPrice){
+            totalPayed += totalPerMonth;
+            totalInstallments++;
+        }
+        System.out.println("Total of Installments = "+ totalInstallments);
+    }
+}
